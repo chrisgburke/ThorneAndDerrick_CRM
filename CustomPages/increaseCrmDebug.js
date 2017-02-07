039c0895644ab9dd978bcf66e6f9e0d820bdecb3
Chris Burke <chris@increase.co.uk>
2017-02-07 16:11:16 +0000

    Debug Logging :
    Improved the logging in the increaseCrmDebug file

--- CustomPages/increaseCrmDebug.js
+++ CustomPages/increaseCrmDebug.js
@@ -29,14 +29,30 @@ function ScreenLoggingOn() {
 function Log(message) {
 	if(ASPLoggingOn()){
         try {  
-            var fso = new ActiveXObject("Scripting.FileSystemObject");
-            var s = fso.OpenTextFile(logFileName(), 8, true);
+			var log_path = Server.MapPath("Logs") + "\\"; 
+			var log_name = logFileName();
+			var fso = new ActiveXObject("Scripting.FileSystemObject");
+            if( !fso.FileExists(log_path + log_name)){
+				var newFile = fso.CreateTextFile(  log_path + log_name );
+        		newFile.Close();
+        		newFile = null;
+			}
+			fso = null;
+			fso = new ActiveXObject("Scripting.FileSystemObject");
+			var s = fso.OpenTextFile(log_path + log_name, 8, true);
             s.writeline("< " + new Date().toTimeString() + " - " + message + " >");
             s.writeline("");
             s.Close();
             fso = null;
         }catch(error){
             Response.Write(error.message);
+			throw {
+				name:'AspLoggingError',
+				message: message,
+				remedy : function(){
+					Response.Write(message);
+				}
+			}
         }        
     }		
 }
@@ -58,7 +74,16 @@ function LogErrorToScreen(message) {
 
 //This is the path and filename of the ASP log file
 function logFileName() {
-    return "D:\\CRM2017\\CRM2017\\Logs\\aspScriptLog.txt";
+    //return "D:\\CRM2017\\CRM2017\\Logs\\aspScriptLog.txt";
+	return "thing.txt";
+}
+
+function SleepMainThread(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
 }
 
 %>
