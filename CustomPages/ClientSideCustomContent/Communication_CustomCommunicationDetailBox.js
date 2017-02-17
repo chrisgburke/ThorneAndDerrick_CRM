c710f7cd8821cb258580a769f39e3f17a0407b99
Chris Burke <chris@increase.co.uk>
2017-02-17 13:42:03 +0000

    Changed the 'to follow' task: ...so it doesn't need to make AJAX call - we simply set the comp/person/quote and hide the other fields

--- CustomPages/ClientSideCustomContent/Communication_CustomCommunicationDetailBox.js
+++ CustomPages/ClientSideCustomContent/Communication_CustomCommunicationDetailBox.js
@@ -13,52 +13,57 @@ $(document).ready(function () {
 
 function doAction361FollowOn() {
 
-    var compSet, perSet, quotSet;
+    $("#comm_subject").focus();
+
+    var compSet = true, perSet = false, quotSet = false;
     var baseURL = increaseCrmLib.MakeRequestString("GetCommunicationQuoteData", "");
 
     var compID = crm.getArg("Key1");
+    $("#_HIDDENcmli_comm_companyid").val(compID);
+    $("#cmli_comm_companyid").val(compID);
+    // increaseCrmLib.MakeSimpleAsyncAjaxRequest(baseURL + "&reqType=c&recordID=" + compID, function (data) {
+    //     $("#cmli_comm_companyidTEXT").val(data);
+    //     $("#SearchSmallAdvcmli_comm_companyid").click();       
+    // },
+    //     function (e) {
 
-    increaseCrmLib.MakeSimpleAsyncAjaxRequest(baseURL + "&reqType=c&recordID=" + compID, function (data) {
-        $("#cmli_comm_companyidTEXT").val(data);
-        $("#SearchSmallAdvcmli_comm_companyid").click();
-    },
-        function (e) {
-
-        });
+    //     });
 
 
 
     var persID = crm.getArg("Key2");
-    increaseCrmLib.MakeSimpleAsyncAjaxRequest(baseURL + "&reqType=p&recordID=" + persID, function (data) {
-        $("#cmli_comm_personidTEXT").val(data);
-        $("#SearchSmallAdvcmli_comm_personid").click();
-    },
-        function (e) {
+    $("#_HIDDENcmli_comm_personid").val(persID);
+    $("#cmli_comm_personid").val(persID);
+    //     increaseCrmLib.MakeSimpleAsyncAjaxRequest(baseURL + "&reqType=p&recordID=" + persID, function (data) {
+    //         if ($("#cmli_comm_personidTEXT").val() === ""){
+    //         $("#cmli_comm_personidTEXT").val(data);
+    //         $("#SearchSmallAdvcmli_comm_personid").click();
+    //     }
+    // },
+    // function (e) {
 
-        });
+    // });
 
     var quoteID = crm.getArg("Key86");
-    increaseCrmLib.MakeSimpleAsyncAjaxRequest(baseURL + "&reqType=q&recordID=" + quoteID, function (data) {
-        $("#_HIDDENcomm_opportunityid").val(quoteID);
-        $("#comm_opportunityid").val(quoteID);
-        $("#_HIDDENSearchFieldcomm_opportunityid").val("comm_quoteid");
-        $("#_HIDDENcomm_opportunityidTEXT").val(data);
-        $("#comm_opportunityidTEXT").val(data);
-    },
-        function (e) {
+    $("#_HIDDENcomm_opportunityid").val(quoteID);
+    $("#comm_opportunityid").val(quoteID);
+    $("#_HIDDENSearchFieldcomm_opportunityid").val("comm_quoteid");
+    // increaseCrmLib.MakeSimpleAsyncAjaxRequest(baseURL + "&reqType=q&recordID=" + quoteID, function (data) {
+    //     $("#_HIDDENcomm_opportunityid").val(quoteID);
+    //     $("#comm_opportunityid").val(quoteID);
+    //     $("#_HIDDENSearchFieldcomm_opportunityid").val("comm_quoteid");
+    //     $("#_HIDDENcomm_opportunityidTEXT").val(data);
+    //     $("#comm_opportunityidTEXT").val(data);
 
-        });
-    // crm.sdata({
-    //     entity: "quotes",
-    //     id: quoteID,
-    //     success: function (d) {
-    //         $("#_HIDDENcomm_opportunityid").val(quoteID);
-    //         $("#comm_opportunityid").val(quoteID);
-    //         $("#_HIDDENSearchFieldcomm_opportunityid").val("comm_quoteid");
-    //         $("#_HIDDENcomm_opportunityidTEXT").val(d.quot_reference);
-    //         $("#comm_opportunityidTEXT").val(d.quot_reference);
-    //     }
-    // });
+    // },
+    //     function (e) {
+
+    //     });
+
+    //$("#EntryForm > table").hide();
+    $("#EntryForm  tr.GridHeader:first").hide();
+    $("#EntryForm  tr.CONTENT:first").hide();
+
 }
 
 function doAction546() {
@@ -75,3 +80,4 @@ function makeHtml() {
     var htmlStr = '<tr class="VIEWBOXCAPTION"><td><input type="checkbox" id="DoFollowUpTask" name="DoFollowUpTask" value="Y" onclick="javascript:if (checked) document.EntryForm.DoFollowUpAppt.checked=!checked;"></td><td><div style="cursor:hand" onclick="javascript:document.EntryForm.DoFollowUpTask.click();">Create Follow-up Task</div></td></tr>';
     return htmlStr;
 }
+
