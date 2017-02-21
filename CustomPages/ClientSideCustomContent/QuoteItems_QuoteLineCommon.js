227332e7c114e72087f590e4facb49ee1fd688ac
Chris Burke <chris@increase.co.uk>
2017-02-21 08:22:50 +0000

    Linting

--- CustomPages/ClientSideCustomContent/QuoteItems_QuoteLineCommon.js
+++ CustomPages/ClientSideCustomContent/QuoteItems_QuoteLineCommon.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     SetCurrency();
 
     PerformLineCalculations();
 
-    $("#quit_salesprice_CID").on("change", function(){
+    $("#quit_salesprice_CID").on("change", function () {
         PerformLineCalculations();
     });
 
 });
 
-function OnProductFamilySelectedCommon(){
- var parentEntryForm = getParentEntryForm('quit_productfamilyid');
-    if(parentEntryForm.quit_productfamilyid.value != ''){
+function OnProductFamilySelectedCommon() {
+    var parentEntryForm = getParentEntryForm('quit_productfamilyid');
+    if (parentEntryForm.quit_productfamilyid.value != '') {
         var fam = $("#_HIDDENquit_productfamilyidTEXT").val();
         PerformLineCalculations(fam);
     }
@@ -26,9 +26,9 @@ function PerformLineCalculations(fam) {
         line.productFamily = $("#_HIDDENquit_productfamilyidTEXT").val();
     }
     line.isCarriage = false;
-    if(_.indexOf(window.carriageCodesLookup, line.productFamily) != -1){
+    if (_.indexOf(window.carriageCodesLookup, line.productFamily) != -1) {
         line.isCarriage = true;
-    } 
+    }
     line.quantity = coalesceZero(crm.fields("quit_quantity").val(), 0);
     line.salesPrice = coalesceZero(crm.fields("quit_salesprice").val(), 2);
     line.costPrice = coalesceZero(crm.fields("quit_cost").val(), 2);
@@ -46,15 +46,15 @@ function PerformLineCalculations(fam) {
 
 }
 
-function SetLineTotal(){
+function SetLineTotal() {
 
-//line total net:
-    var lineTotal = quoteLineValueCoordinator.getLineTotal();   
+    //line total net:
+    var lineTotal = quoteLineValueCoordinator.getLineTotal();
     setTextInDataSpan("_Dataquit_linetotalnet", lineTotal, true);
     $("#_HIDDENquit_linetotalnet").val(lineTotal);
     $("#_HIDDENquit_linetotalnet_CID").val(quoteLineValueCoordinator.getCurrencyID());
 
-//line item discount:
+    //line item discount:
     var lineItemDiscount = quoteLineValueCoordinator.getLineItemDiscount();
     setTextInDataSpan("_Dataquit_itemlinediscount", lineItemDiscount, true);
     $("#_HIDDENquit_itemlinediscount").val(lineItemDiscount);
@@ -62,20 +62,20 @@ function SetLineTotal(){
 
 }
 
-function SetVATValues(){
+function SetVATValues() {
 
     var vatAmount = quoteLineValueCoordinator.getTaxAmount();
     setTextInDataSpan("_Dataquit_vatamount", vatAmount, true);
     $("#_HIDDENquit_vatamount").val(vatAmount);
 }
 
-function SetProfitValues(){
+function SetProfitValues() {
 
     var profitValue = quoteLineValueCoordinator.getProfitValue();
     setTextInDataSpan("_Dataquit_profitvalue", profitValue, true);
     $("#_HIDDENquit_profitvalue").val(profitValue);
     $("#_HIDDENquit_profitvalue_CID").val(quoteLineValueCoordinator.getCurrencyID());
-    
+
     var profitMargin = quoteLineValueCoordinator.getProfitMargin();
     setTextInDataSpan("_Dataquit_profitmargin", profitMargin, false);
     $("#_HIDDENquit_profitmargin").val(profitMargin);
@@ -113,17 +113,19 @@ function coalesceZero(input, dp) {
 
 function setTextInDataSpan(spanID, newValue, convert) {
     var newTextValue = "";
-    if(convert){
+    if (convert) {
         newTextValue = quoteLineValueCoordinator.formatWithCurrencySymbol(newValue);
-    }else{
+    } else {
         newTextValue = newValue;
     }
     var your_div = document.getElementById(spanID);
-    var text_to_change = your_div.childNodes[0];
-    text_to_change.nodeValue = newTextValue;
+    if (your_div) {
+        var text_to_change = your_div.childNodes[0];
+        text_to_change.nodeValue = newTextValue;
+    }
 }
 
-function SetCurrency(){
+function SetCurrency() {
     var currencyOnQuote = $("#_HIDDENquot_currency").val();
     var setter = "#quit_salesprice_CID option[value=" + currencyOnQuote + "]";
     $(setter).attr("selected", "selected");
