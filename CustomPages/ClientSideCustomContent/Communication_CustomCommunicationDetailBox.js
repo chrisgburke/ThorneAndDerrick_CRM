4dbd0d5fb52d2634ff338070e53a01ac0ae15187
Chris Burke <Chris Burke@INC.local>
2017-09-07 08:19:52 +0100

    Clean-up commit

--- CustomPages/ClientSideCustomContent/Communication_CustomCommunicationDetailBox.js
+++ CustomPages/ClientSideCustomContent/Communication_CustomCommunicationDetailBox.js
@@ -1,17 +1,44 @@
 $(document).ready(function () {
 
+    var doLogging = true;
+    var _log;
+
+    // if (typeof (log4javascript) != "undefined") {
+
+    //     // Create the logger
+    //     _log = log4javascript.getLogger();
+
+    //     // Create a PopUpAppender with default options
+    //     var popUpAppender = new log4javascript.PopUpAppender();
+    //     popUpAppender.setThreshold(log4javascript.Level.DEBUG);
+    //     // Change the desired configuration options
+    //     popUpAppender.setFocusPopUp(true);
+    //     popUpAppender.setNewestMessageAtTop(true);
+
+    //     // Add the appender to the logger
+    //     _log.addAppender(popUpAppender);
+
+    //     // Test the logger
+    //     _log.debug("LOG START");
+    // }
+
     var arg = crm.getArg("Act");
     var dft = crm.getArg("DFT");
+
+    writeToLog(_log, "Act = " + arg + " DFT = " + dft);
+
     if (arg == "546") {
-        doAction546();
+        doAction546(_log);
     }
     if (arg == "361" && dft == "1") {
-        doAction361FollowOn();
+        doAction361FollowOn(_log);
     }
 });
 
 
-function doAction361FollowOn() {
+function doAction361FollowOn(_log) {
+
+    writeToLog(_log, "doAction361FollowOn START");
 
     $("#comm_subject").focus();
 
@@ -21,63 +48,51 @@ function doAction361FollowOn() {
     var compID = crm.getArg("Key1");
     $("#_HIDDENcmli_comm_companyid").val(compID);
     $("#cmli_comm_companyid").val(compID);
-    // increaseCrmLib.MakeSimpleAsyncAjaxRequest(baseURL + "&reqType=c&recordID=" + compID, function (data) {
-    //     $("#cmli_comm_companyidTEXT").val(data);
-    //     $("#SearchSmallAdvcmli_comm_companyid").click();       
-    // },
-    //     function (e) {
-
-    //     });
-
 
 
     var persID = crm.getArg("Key2");
     $("#_HIDDENcmli_comm_personid").val(persID);
     $("#cmli_comm_personid").val(persID);
-    //     increaseCrmLib.MakeSimpleAsyncAjaxRequest(baseURL + "&reqType=p&recordID=" + persID, function (data) {
-    //         if ($("#cmli_comm_personidTEXT").val() === ""){
-    //         $("#cmli_comm_personidTEXT").val(data);
-    //         $("#SearchSmallAdvcmli_comm_personid").click();
-    //     }
-    // },
-    // function (e) {
 
-    // });
 
     var quoteID = crm.getArg("Key86");
     $("#_HIDDENcomm_opportunityid").val(quoteID);
     $("#comm_opportunityid").val(quoteID);
     $("#_HIDDENSearchFieldcomm_opportunityid").val("comm_quoteid");
-    // increaseCrmLib.MakeSimpleAsyncAjaxRequest(baseURL + "&reqType=q&recordID=" + quoteID, function (data) {
-    //     $("#_HIDDENcomm_opportunityid").val(quoteID);
-    //     $("#comm_opportunityid").val(quoteID);
-    //     $("#_HIDDENSearchFieldcomm_opportunityid").val("comm_quoteid");
-    //     $("#_HIDDENcomm_opportunityidTEXT").val(data);
-    //     $("#comm_opportunityidTEXT").val(data);
 
-    // },
-    //     function (e) {
-
-    //     });
-
-    //$("#EntryForm > table").hide();
     $("#EntryForm  tr.GridHeader:first").hide();
     $("#EntryForm  tr.CONTENT:first").hide();
 
+    writeToLog(_log, "doAction361FollowOn COMPLETE");
 }
 
-function doAction546() {
+function doAction546(_log) {
+
+    writeToLog(_log, "doAction546 START");
+
     var trToAddTo = $("#_HIDDENcomm_percentcomplete").parent();
     trToAddTo.append(makeHtml());
 
     var formAction = $("#StandardForm").attr("action");
     formAction += "&DFT=1";
+
+    writeToLog(_log, "Form Action is : " + formAction);
+
     $("#StandardForm").attr("action", formAction);
+
+    writeToLog(_log, $("#StandardForm").attr("action"));
+    writeToLog(_log, "doAction546 COMPLETE");
 }
 
 function makeHtml() {
 
     var htmlStr = '<tr class="VIEWBOXCAPTION"><td><input type="checkbox" id="DoFollowUpTask" name="DoFollowUpTask" value="Y" onclick="javascript:if (checked) document.EntryForm.DoFollowUpAppt.checked=!checked;"></td><td><div style="cursor:hand" onclick="javascript:document.EntryForm.DoFollowUpTask.click();">Create Follow-up Task</div></td></tr>';
     return htmlStr;
+}
+
+function writeToLog(_log, msg) {
+    if (typeof(_log) != "undefined") {
+        _log.debug(msg);
+    }
 }
 
